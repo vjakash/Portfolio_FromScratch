a9a61ca032a5f9e6eba434bbe0462ac2bc2a5006
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-07-07 13:59:29 +0530

    done mobile responsive

---
+++ index.js
@@ -0,0 +1,29 @@
+let currentTopic = "home";
+let nav = {}
+window.onload = () => {
+    nav = {
+        "home": document.getElementById("home"),
+        "about": document.getElementById("about"),
+        "skills": document.getElementById("skills"),
+        "project": document.getElementById("project"),
+        "contact": document.getElementById("contact"),
+    }
+    nav["about"].remove();
+    nav["skills"].remove();
+    nav["project"].remove();
+    nav["contact"].remove();
+}
+
+function changeMain(topic) {
+    console.log(topic, currentTopic);
+    // console.log(nav[topic]);
+    document.getElementById(currentTopic).remove();
+    document.getElementById("root").appendChild(nav[topic]);
+    currentTopic = topic;
+    document.getElementById('navbar').classList.toggle('active');
+}
+
+
+document.getElementById("hamburger").addEventListener('click', function(e) {
+    document.getElementById('navbar').classList.toggle('active');
+})
