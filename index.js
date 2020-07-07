6d49d5c7d7909903adb692c82ac167b7bda68426
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-07-07 16:29:41 +0530

    .

--- index.js
+++ index.js
@@ -18,15 +18,19 @@ function changeMain(topic) {
     document.getElementById(currentTopic).remove();
     document.getElementById("root").appendChild(nav[topic]);
     currentTopic = topic;
+    // document.getElementById('navbar').classList.toggle('inactive');
     document.getElementById('navbar').classList.toggle('active');
     document.getElementById(currentTopic).addEventListener('click', function(e) {
         document.getElementById('navbar').classList.remove('active');
+        // document.getElementById('navbar').classList.add('inactive');
     })
 }
 document.getElementById(currentTopic).addEventListener('click', function(e) {
     document.getElementById('navbar').classList.remove('active');
+    // document.getElementById('navbar').classList.toggle('inactive');
 })
 
 document.getElementById("hamburger").addEventListener('click', function(e) {
     document.getElementById('navbar').classList.toggle('active');
+    // document.getElementById('navbar').classList.toggle('inactive');
 })
